770238b6902275e773f0982a0dd2fc41933d060c
nke69 <nookie69@gmail.com>
2018-04-14 13:45:16 +0200

    new commit

---
+++ Misc/FloatSwitch/FloatSwitch.ino
@@ -0,0 +1,43 @@
+/*
+Jared Anwyl
+Turns on and off a light emitting diode(LED) connected to digital  
+pin 13, when using a float switch attached to pin 2.
+
+The circuit:
+* LED attached from pin 13 to ground 
+* Float Switch attached to pin 2 from +5V
+* 10K resistor attached to pin 2 from ground
+
+This example code is in the public domain.
+*/
+
+// constants won't change. They're used here to 
+// set pin numbers:
+const int floatPin = 7;     // the number of the Float pin
+const int ledPin =  8;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);      
+  // initialize the pushbutton pin as an input:
+  pinMode(floatPin, INPUT);     
+}
+
+void loop(){
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(floatPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == LOW) {     
+    // turn LED on:    
+    digitalWrite(ledPin, HIGH);  
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW); 
+  }
+}
