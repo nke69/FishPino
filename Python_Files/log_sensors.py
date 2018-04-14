1125bbaceb04362b74e6292d9004139ddbee3db0
nke69 <nookie69@gmail.com>
2018-04-14 13:58:40 +0200

    new python file serialusb to grab data

---
+++ Python_Files/log_sensors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import serial
+
+device = '/dev/ttyACM0' #this will have to be changed to the serial port you are using
+
+try:
+  print ("Trying..."),device 
+  arduino = serial.Serial(device, 9600) 
+
+except: 
+  print ("Failed to connect on"),device  
+
+data = arduino.readline()
+pieces =data.split("\t")
+getTemp = pieces[0]
+phValue = pieces[1]
+waterlevel = pieces[2]
+hum = pieces[3]
+temp = pieces[4]
+
+with open("/path_to_directory/sensors.json","w+") as sensordata: sensordata.write('{"temperature": %s, "pH": %s, "niveau": %s, "humidite": %s, "tempext": %s}' % (pieces[0],pieces[1],pieces[2],pieces[3],pieces[4]))
+
+print pieces[0],pieces[1],pieces[2],pieces[3],pieces[4]
