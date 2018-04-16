608b1f6389afd3d00a292eaa558c0b71c81a4693
nke69 <nookie69@gmail.com>
2018-04-16 07:25:03 +0200

    Replace some values

--- Arduino_Files/FishPiNo/FishPiNo.ino
+++ Arduino_Files/FishPiNo/FishPiNo.ino
@@ -1,68 +1,68 @@
 /*********************************************************************
-   FishPiNo.ino
-   Hardware platform   : Arduino Uno
-
-   Description:
-   Aquarium temperature (heating and cooling)) and pH-controller.
-   Waterlevel indicator controls a cooling-fan, aquarium-heater and co2 system magnet valve.
-   LCD Display for alert and views.
-
-   Sensor pin:
-
-   DIGITAL:
-   LCD            : D2 - D3 - D4 - D5 - D11 - D12
-   LEDUP          : D6  // led warning temp. upper limit or cooling Fan relais!
-   WATERPIN       : D7  // floatswitch  for waterlevel
-   LEDWATER       : D8  // warning led for water level
-   DS18B20        : D9  // Signal pin on digital 9 temp sensor
-   LEDPH          : D10 // led warning ph or relais for magnetic valve co2-system
-   DHT22          : D13 // pin for DHT22
-
-   ANALOG:
-   dtempPin       : A1  //Lower temp.Limit pin with potmeter
-   utempPin       : A2  //Upper temp.limit pin with potmeter
-   SensorPin PH   : A3  //pH meter Analog output to Arduino Analog Input 3
-   phsetPin       : A4  //set desired ph-value with potmeter
-   HEATER         : A5  // led warning temp on  or RELAY FOR HEATER
-
-   Free PIN:
-   A0
-
-   Author  :  nke69(nookie69@gmail.com)
-   Version :  V1.0
-   Date    :  31-03-2018
-
-   Explanation :
-
-   Controlling Aquarium temperature via heating and cooling relay, pH- controller and waterlevel indicator controls a cooling-fan,
-   aquarium-heater and co2 system magnet valve.
-   I used a 4 relay board, used 3 relay so you can do without the 3 leds.(those were for testing).
-   Make sure the relay board has its own power-source (1x power adapter 12volt 1 amp,and 2x 7805 5 volt regulator.
-   1x 7805 for arduino pro mini and sensors and and lcd, 1x 7805 for relay board.
-
-   Parts list:
-
-   1x arduino pro mini 16 mhz 5 volts with atmega 328.
-   1x ph sensor  PH meterSKU: SEN0161, amplifier board included.
-   1x DS18B20 one wire digital temperatur sensor, watersealed.
-   1x 4 channel relais board.5 volts
-   4x potentiometer 10 k for phset, maxtempset, mintempset and lcd-contrast.
-   1x 16x2 lcd.
-   1x power adapter 12 volt 1 amp.
-   1x powerplug for housing to connect poweradapter to.
-   2x 7805 volt regulator ,make a small pcb board with those two and the needed capacitors,to make 2 seperated 5 volts supplies, one for the relais board,one for the rest.
-   1x led or blinkled for waterlevel warning indicator.
-   1x 3 screw connector for temp sensor, 1x 2 screw connector for float switch sensor
-   1x float sensor switch Water Level Liquid Sensor.
-   2x build-on wall sockets ,both on 220 volts mains,here controlled by two relais,1 for heater and 1 for magnetvalve.
-   1x pushbutton,connected to reset.
-
-   Tips:
-
-   Sometimes lcd can get scrambled because of the relais switching or the switching of my TL lighting, 1 push on //reset to fix it.
-   a small condenator (100nF-10uF,trial and error) directly soldered over the lcd power pins(VSS and VDD) might help.
-   if lcd is crambled the controller still work properly,lcd is just for indication!
-
+ * FishPiNo.ino
+ * Hardware platform   : Arduino Uno
+ * 
+ * Description:
+ * Aquarium temperature (heating and cooling)) and pH-controller.
+ * Waterlevel indicator controls a cooling-fan, aquarium-heater and co2 system magnet valve.
+ * LCD Display for alert and views.
+ * 
+ * Sensor pin:
+ * 
+ * DIGITAL:
+ * LCD            : D2 - D3 - D4 - D5 - D11 - D12
+ * LEDUP          : D6  // led warning temp. upper limit or cooling Fan relais!
+ * WATERPIN       : D7  // floatswitch  for waterlevel
+ * LEDWATER       : D8  // warning led for water level
+ * DS18B20        : D9  // Signal pin on digital 9 temp sensor
+ * LEDPH          : D10 // led warning ph or relais for magnetic valve co2-system
+ * DHT22          : D13 // pin for DHT22
+ * 
+ * ANALOG:
+ * dtempPin       : A1  //Lower temp.Limit pin with potmeter
+ * utempPin       : A2  //Upper temp.limit pin with potmeter
+ * SensorPin PH   : A3  //pH meter Analog output to Arduino Analog Input 3
+ * phsetPin       : A4  //set desired ph-value with potmeter
+ * HEATER         : A5  // led warning temp on  or RELAY FOR HEATER
+ * 
+ * Free PIN:
+ * A0
+ * 
+ * Author  :  nke69(nookie69@gmail.com)
+ * Version :  V1.0
+ * Date    :  31-03-2018
+ * 
+ * Explanation :
+ * 
+ * Controlling Aquarium temperature via heating and cooling relay, pH- controller and waterlevel indicator controls a cooling-fan,
+ * aquarium-heater and co2 system magnet valve.
+ * I used a 4 relay board, used 3 relay so you can do without the 3 leds.(those were for testing).
+ * Make sure the relay board has its own power-source (1x power adapter 12volt 1 amp,and 2x 7805 5 volt regulator.
+ * 1x 7805 for arduino pro mini and sensors and and lcd, 1x 7805 for relay board.
+ * 
+ * Parts list:
+ * 
+ * 1x arduino pro mini 16 mhz 5 volts with atmega 328.
+ * 1x ph sensor  PH meterSKU: SEN0161, amplifier board included.
+ * 1x DS18B20 one wire digital temperatur sensor, watersealed.
+ * 1x 4 channel relais board.5 volts
+ * 4x potentiometer 10 k for phset, maxtempset, mintempset and lcd-contrast.
+ * 1x 16x2 lcd.
+ * 1x power adapter 12 volt 1 amp.
+ * 1x powerplug for housing to connect poweradapter to.
+ * 2x 7805 volt regulator ,make a small pcb board with those two and the needed capacitors,to make 2 seperated 5 volts supplies, one for the relais board,one for the rest.
+ * 1x led or blinkled for waterlevel warning indicator.
+ * 1x 3 screw connector for temp sensor, 1x 2 screw connector for float switch sensor
+ * 1x float sensor switch Water Level Liquid Sensor.
+ * 2x build-on wall sockets ,both on 220 volts mains,here controlled by two relais,1 for heater and 1 for magnetvalve.
+ * 1x pushbutton,connected to reset.
+ * 
+ * Tips:
+ * 
+ * Sometimes lcd can get scrambled because of the relais switching or the switching of my TL lighting, 1 push on //reset to fix it.
+ * a small condenator (100nF-10uF,trial and error) directly soldered over the lcd power pins(VSS and VDD) might help.
+ * if lcd is crambled the controller still work properly,lcd is just for indication!
+ * 
  **********************************************************************/
 
 #include <Adafruit_Sensor.h>
@@ -88,7 +88,6 @@ int utemp = 0, dtemp = 0, valup = 0, valdown = 0;
 
 //Temperature chip i/o
 OneWire ds(DS18S20_Pin);     // ds18b20 on digital pin 9
-
 const int dtempPin = A1;     //Lower temp.Limit pin with potmeter
 const int utempPin = A2;     //Upper temp.limit pin with potmeter
 #define phsetPin A4          //set desired ph-value with potmeter
@@ -98,10 +97,11 @@ const int ledph = 10;        //led warning ph or relais for magnetic valve co2-s
 const int ledwater = 8;      //warning led for water level
 const int heat = A5;         //relais heater element
 #define SensorPin A3         //pH meter Analog output to Arduino Analog Input 3
-#define Offset -0.40           //set your offset ph sensor here!!! shorted then ph=7 ,here mine gives 6.8 so offset = 0.2
+#define Offset -0.40         //set your offset ph sensor here!!! shorted then ph=7 ,here mine gives 6.8 so offset = 0.2
 unsigned long int avgValue;  //Store the average value of the sensor feedback
 float b;
 int buf[10], temp;
+
 byte flame[8] = {
   B00100, B00100, B01110, B01010, B11011, B10001, B10001, B01110
 };       //icon for flame
@@ -264,19 +264,19 @@ void loop()
       Serial.print(waterlevel);
       Serial.print(" \t"); //tab
       delay(1000);
-      
+
       /*
       lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Humidite : ");
-      lcd.setCursor(11, 0);
-      lcd.print(hum);
-      lcd.setCursor(0, 1);
-      lcd.print("Temp.air : ");
-      lcd.setCursor(11, 1);
-      lcd.print(temp, 2);
-      delay(3000);
-      */
+       lcd.setCursor(0, 0);
+       lcd.print("Humidite : ");
+       lcd.setCursor(11, 0);
+       lcd.print(hum);
+       lcd.setCursor(0, 1);
+       lcd.print("Temp.air : ");
+       lcd.setCursor(11, 1);
+       lcd.print(temp, 2);
+       delay(3000);
+       */
 
       // check if returns are valid, if they are NaN (not a number) then something went wrong!
 
@@ -406,5 +406,3 @@ float getTemp() {
 
   return TemperatureSum;
 }
-
-
